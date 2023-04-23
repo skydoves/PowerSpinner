39a12a89b3f4c9f66cb80afbadffb38c9828ef3c
skydoves <skydoves@naver.com>
2023-04-23 21:07:24 +0200

    Bump lifecycle to 2.6.0

--- buildSrc/src/main/kotlin/com/skydoves/powerspinner/Dependencies.kt
+++ buildSrc/src/main/kotlin/com/skydoves/powerspinner/Dependencies.kt
@@ -3,14 +3,14 @@ package com.skydoves.powerspinner
 object Versions {
     internal const val ANDROID_GRADLE_PLUGIN = "7.4.1"
     internal const val ANDROID_GRADLE_SPOTLESS = "6.7.0"
-    internal const val GRADLE_NEXUS_PUBLISH_PLUGIN = "1.1.0"
+    internal const val GRADLE_NEXUS_PUBLISH_PLUGIN = "1.3.0"
     internal const val KOTLIN = "1.8.10"
-    internal const val KOTLIN_GRADLE_DOKKA = "1.7.20"
+    internal const val KOTLIN_GRADLE_DOKKA = "1.8.10"
     internal const val KOTLIN_BINARY_VALIDATOR = "0.13.0"
 
     internal const val APPCOMPAT = "1.6.1"
     internal const val MATERIAL = "1.8.0"
-    internal const val LIFECYCLE = "2.5.1"
+    internal const val LIFECYCLE = "2.6.0"
     internal const val RECYCLERVIEW = "1.2.1"
     internal const val PREFERENCE = "1.2.0"
 }
